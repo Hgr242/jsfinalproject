32fa5dafe31a6a554635f5c45d0ecf32714ef18f
Hgr242 <96385961+Hgr242@users.noreply.github.com>
2022-05-26 07:02:52 +0200

    Update app.js

--- finalproject-w19/app.js
+++ finalproject-w19/app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar-menu');
+const navLogo = document.querySelector('#navbar-logo')
 const mobileMenu = () => {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
